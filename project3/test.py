b0fde2f4344b1e9263fce43a187ba74d96049689
KomK2 <ksrikiran1@gmail.com>
2024-03-24 20:49:39 -0400

    added step size

--- project3/test.py
+++ project3/test.py
@@ -7,8 +7,9 @@ import math
 
 # Node class
 class Node :
-    def __init__ (self, points, parent, cost, heuristic):
+    def __init__ (self, points, orientation,  parent, cost, heuristic):
         self.points = points
+        self.orientation = orientation
         self.parent = parent
         self.cost = cost
         self.heuristic = heuristic
@@ -16,6 +17,9 @@ class Node :
 
     def getPoints(self):
         return self.points
+    
+    def getOrientation(self):
+        return self.orientation
     
     def getParent(self):
         return self.parent
@@ -35,6 +39,16 @@ goal = None
 start_point = None
 canvas = np.ones((500, 1200, 3), dtype=np.uint8 )
 new_color = (255, 0, 0)
+clearance = 0
+stepSize = 0
+
+def initalize_varaibles():
+    global clearance
+    global stepSize
+    clearance = int (input("Enter the clearance in: "))
+    stepSize = int (input("Enter the step size between 1 and 10: "))
+
+
 
 def hexagon_vertex() :
     hexagon_vertex = []
@@ -50,15 +64,19 @@ hexagon_points = np.array(hexagon_vertex())
 u_shape = np.array([[1200-100-200,y-(450)],[1200-100,y-450],[1200-100,y-50],[1200-100-200,y-(50)],[1200-100-200,y-75-50],[1200-100-200+120,y-75-50],[1200-100-200+120,y-(375)],[1200-100-200,y-375] ])
 
 cv2.polylines(canvas, [hexagon_points], True, (255, 0, 255), 5)
+cv2.polylines(canvas, [hexagon_points], True, (255, 0, 255), clearance)
 cv2.fillPoly(canvas, [hexagon_points], (0, 0, 255))
 
 cv2.rectangle(canvas, (100, y-500), (175, y-100), (255, 0, 255), 5 )
+cv2.rectangle(canvas, (100, y-500), (175, y-100), (0, 0, 255), clearance )
 cv2.rectangle(canvas, (100, y-500), (175, y-100), (0, 0, 255), -1)
 
 cv2.rectangle(canvas, (100+75+100, y-400), (100+75+100+75, y), (255, 0, 255), 5)
+cv2.rectangle(canvas, (100+75+100, y-400), (100+75+100+75, y), (0, 0, 255), clearance)
 cv2.rectangle(canvas, (100+75+100, y-400), (100+75+100+75, y), (0, 0, 255), -1)
 
 cv2.polylines(canvas, [u_shape], True, (255, 0, 255), 5)
+cv2.polylines(canvas, [u_shape], True, (255, 0, 255), clearance)
 cv2.fillPoly(canvas, [u_shape],(0, 0, 255))
 
 
@@ -102,119 +120,151 @@ def canMove(point):
 
 def start_end_goals():
     initial_point = int(input("Enter the x coordinate of the initial point: ")), int(input("Enter the y coordinate of the initial point: "))
+    inital_orentation = int(input("Enter the orientation of robot at initial point: "))
+
     goal_point = int(input("Enter the x coordinate of the goal point: ")), int(input("Enter the y coordinate of the goal point: "))
+    goal_orentation = int(input("Enter the orientation of robot at goal point: "))
+
     if canMove(initial_point) and canMove(goal_point):
-        return initial_point, goal_point
+        return initial_point, goal_point , inital_orentation, goal_orentation
     else:
         print("Invalid points. Please enter valid points.")
         return start_end_goals()
 
-def moveUP(node):
+def moveStraight(node,step_size):
     current = node.getPoints()
-    newPoints = (current[0],current[1]+1)
-    if not canMove(newPoints):
-        return False , None ,0 
-    return True, newPoints, 1
+    orientation = node.getOrientation()
 
-def moveDOWN(node):
+    new_x = current[0] + int(round((math.cos(math.radians(orientation))*step_size )))
+    new_y = current[1] + int(round((math.sin(math.radians(orientation))*step_size)))
+
+    newPoints = (new_x, new_y)
+    if not canMove(newPoints):
+        return False , None ,None , step_size
+    return True, newPoints, orientation, step_size    
+
+def move_left_30(node,step_size):
     current = node.getPoints()
-    newPoints = (current[0],current[1]-1)
-    if not canMove(newPoints):
-        return False , None ,0
-    return True, newPoints , 1
+    orientation = node.getOrientation()
 
-def moveLEFT(node):
+    new_x = current[0] + int(round((math.cos(math.radians(orientation-30))*step_size )))
+    new_y = current[1] + int(round((math.sin(math.radians(orientation-30))*step_size)))
+
+    newPoints = (new_x, new_y)
+    if not canMove(newPoints):
+        return False , None ,None , step_size
+    return True, newPoints, orientation-30, step_size
+
+def move_left_60(node,step_size):
     current = node.getPoints()
-    newPoints = (current[0]-1,current[1])
-    if not canMove(newPoints):
-        return False , None ,0
-    return True, newPoints, 1
+    orientation = node.getOrientation()
 
-def moveRIGHT(node):
+    new_x = current[0] + int(round((math.cos(math.radians(orientation-60))*step_size )))
+    new_y = current[1] + int(round((math.sin(math.radians(orientation-60))*step_size)))
+
+    newPoints = (new_x, new_y)
+    if not canMove(newPoints):
+        return False , None ,None , step_size
+    return True, newPoints, orientation-60, step_size
+
+def move_right_30(node,step_size):
     current = node.getPoints()
-    newPoints = (current[0]+1,current[1])
-    if not canMove(newPoints):
-        return False , None ,0
-    return True, newPoints, 1
+    orientation = node.getOrientation()
 
-def moveUPLEFT(node):
+    new_x = current[0] + int(round((math.cos(math.radians(orientation+30))*step_size )))
+    new_y = current[1] + int(round((math.sin(math.radians(orientation+30))*step_size)))
+
+    newPoints = (new_x, new_y)
+    if not canMove(newPoints):
+        return False , None ,None , step_size
+    return True, newPoints, orientation+30, step_size
+
+def move_right_60(node,step_size):
     current = node.getPoints()
-    newPoints = (current[0]-1,current[1]+1)
-    if not canMove(newPoints):
-        return False , None ,0
-    return True, newPoints , 1.4
+    orientation = node.getOrientation()
 
-def moveUPRIGHT(node):
-    current = node.getPoints()
-    newPoints = (current[0]+1,current[1]+1)
-    if not canMove(newPoints):
-        return False , None,0
-    return True, newPoints , 1.4
+    new_x = current[0] + int(round((math.cos(math.radians(orientation+60))*step_size )))
+    new_y = current[1] + int(round((math.sin(math.radians(orientation+60))*step_size)))
 
-def moveDOWNLEFT(node):
-    current = node.getPoints()
-    newPoints = (current[0]-1,current[1]-1)
+    newPoints = (new_x, new_y)
     if not canMove(newPoints):
-        return False , None, 0
-    return True, newPoints, 1.4
+        return False , None ,None , step_size
+    return True, newPoints, orientation+60, step_size
 
-def moveDOWNRIGHT(node):
-    current = node.getPoints()
-    newPoints = (current[0]+1,current[1]-1)
-    if not canMove(newPoints):
-        return False , None ,0
-    return True, newPoints, 1.4
 
+def in_goal_thershold(point,goal, thershold):
+    x , y = point[0] ,point[1]
+    circle_center_x, circle_center_y, = goal[0], goal[1]
+    radius = thershold
+    distance = math.sqrt((x - circle_center_x) ** 2 + (y - circle_center_y) ** 2)
+    return distance <= radius
 
 def heuristic(point1, point2):
     return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
 
-def a_star(initial, final):
+def a_star(initial, final, inital_orentation, goal_orentation ):
     open_list = PriorityQueue()
     closed_list = set()
     visited = dict()
 
-    start_node = Node(initial, None, 0, heuristic(initial, final))
+    start_node = Node(initial, inital_orentation ,None, 0, heuristic(initial, final))
 
     open_list.put((start_node.total_cost, start_node))
     visited[initial] = start_node
     closed_list.add(initial)
     
+
+    
     while not open_list.empty():
         current_node = open_list.get()[1]
         current_point = current_node.getPoints()
 
-        if current_point == final:
+        # if current_point == final:
+        if in_goal_thershold(current_point,final, 1.5):
             path = []
             while current_node is not None:
                 path.append(current_node.getPoints())
                 canvas[current_node.getPoints()[1], current_node.getPoints()[0]] = (255, 255, 0)
                 current_node = current_node.getParent()
+
+                # for i in range(len(path) - 1):
+                #     start_node = path[i]
+                #     end_node = path[i + 1]
+
+                #     # Extracting (x, y) coordinates
+                #     start_x, start_y = start_node[0], start_node[1]
+                #     end_x, end_y = end_node[0], end_node[1]
+
+                #     # Calculating vector components
+                #     vector_x = end_x - start_x
+                #     vector_y = end_y - start_y
+
+                #     # Plotting the vector
+                #     plt.quiver(start_x, start_y, vector_x, vector_y, angles='xy', scale_units='xy', scale=1, color='r')
             return path[::-1]
 
-        for move in [moveUP, moveDOWN, moveLEFT, moveRIGHT, moveUPLEFT, moveUPRIGHT, moveDOWNLEFT, moveDOWNRIGHT]:
-            can_move, new_point, cost = move(current_node)
+        for move in [moveStraight, move_left_30, move_left_60, move_right_30, move_right_60]:
+            can_move, new_point, new_orentation, new_cost = move(current_node,stepSize)
             if can_move:
-                new_node = Node(new_point, current_node, current_node.cost + cost, heuristic(new_point, final))
+                new_node = Node(new_point, new_orentation ,current_node, current_node.cost + new_cost, heuristic(new_point, final))
                 if new_point not in closed_list:
                     if new_point not in visited:
                         open_list.put((new_node.total_cost, new_node))
                         visited[new_point] = new_node
                         closed_list.add(new_point)
+                        
                         canvas[new_point[1], new_point[0]] = new_color
                     else:
                         if visited[new_point].total_cost > new_node.total_cost:
                             visited[new_point] = new_node
                             open_list.put((new_node.total_cost, new_node))
+
     
-
-
-
-
     
 
 
 if __name__ == "__main__":
-    intial, final = start_end_goals()
-    a_star(intial, final)
+    intial, final, inital_orentation, goal_orentation = start_end_goals()
+    initalize_varaibles()
+    a_star(intial, final,inital_orentation, goal_orentation)
     show_image()
